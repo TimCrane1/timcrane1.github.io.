c897787197a664c9fb6610bdcc6a245825cf90bf
Timothy Crane <mickeymouth82@gmail.com>
2018-04-02 17:45:49 -0500

    new

--- js/agency.js
+++ js/agency.js
@@ -1,87 +1,66 @@
 (function($) {
   "use strict"; // Start of use strict
-  
-  // $("body").scroll(function() {
-
-  //   if($('#cor').isInViewport()){
-  //     // corner = true;
-  //     $('#cor').addClass('slideLeft');
-  //   } else if($('#sky').isInViewport()){
-  //     // corner = true;
-  //     $('#sky').addClass('slideRight');
-  //   } else if($('#bud').isInViewport()){
-  //     // corner = true;
-  //     $('#bud').addClass('slideLeft');
-  //   } else if($('#hill').isInViewport()){
-  //     // corner = true;
-  //     $('#hill').addClass('slideRight');
-  //   } else if($('#chq').isInViewport()){
-  //     // corner = true;
-  //     $('#chq').addClass('slideLeft');
-  //   } else if($('#portfolio').isInViewport()){
-  //     // corner = true;
-  //     $('#portfolio').addClass('fade');
-  //   }
-  
-  // });
-  
-
-  // $("#texty").addClass("scrolling");
-  // $("header.masthead").addClass("smaller_mast");
+  $("#texty").addClass("scrolling");
+  console.log('holler');
+  $("body").scroll(function() {
+    console.log('holler2');
+    if($('#portfolio').isInViewport()){
+      // corner = true;
+      $('#portfolio').addClass('fade');
+    } else if($('#about').isInViewport()){
+      // corner = true;
+      $('#about').addClass('fade');
+    } else if($('#team').isInViewport()){
+      // corner = true;
+      $('#team').addClass('fade');
+    } 
+  });
 
   // Smooth scrolling using jQuery easing
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
-      location.hostname == this.hostname
-    ) {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
-        $("html, body").animate(
-          {
-            scrollTop: target.offset().top - 54
-          },
-          1000,
-          "easeInOutExpo"
-        );
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $(".js-scroll-trigger").click(function() {
-    $(".navbar-collapse").collapse("hide");
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $("body").scrollspy({
-    target: "#mainNav",
+  $('body').scrollspy({
+    target: '#mainNav',
     offset: 56
   });
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#page-top").offset().top > 100) {
-      $("#page-top").addClass("navbar-shrink");
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
     } else {
-      $("#page-top").removeClass("navbar-shrink");
+      $("#mainNav").removeClass("navbar-shrink");
     }
   };
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
-//  
+
   // Hide navbar when modals trigger
-  $(".portfolio-modal").on("show.bs.modal", function(e) {
+  $('.portfolio-modal').on('show.bs.modal', function(e) {
     $(".navbar").addClass("d-none");
-  });
-  $(".portfolio-modal").on("hidden.bs.modal", function(e) {
+  })
+  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
     $(".navbar").removeClass("d-none");
-  });
+  })
 
   $.fn.isInViewport = function() {
     var elementTop = $(this).offset().top;
@@ -90,4 +69,5 @@
     var viewportBottom = viewportTop + $(window).height();
     return elementBottom > viewportTop && elementTop < viewportBottom;
   };
+
 })(jQuery); // End of use strict
