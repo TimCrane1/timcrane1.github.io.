edabc80814281d3e77b50b2ea7e5887129dddf5f
Jonathan Furr <jonfurr@gmail.com>
2018-04-11 09:56:51 -0700

    debugging smoothScroll on iOs

--- js/agency.js
+++ js/agency.js
@@ -36,6 +36,8 @@ $(document).ready(function(){
       }
   });
   
+
+
 //   window.addEventListener('resize', function(){
 //     if(window.innerWidth > 568){
        
@@ -44,28 +46,28 @@ $(document).ready(function(){
 
 
   // Smooth scrolling using jQuery easing
-  // $('a[href*="#"]:not([href="#"]:not([href*="#portfolioModal"]))').click(
-  //   function() {
-  //     if (
-  //       location.pathname.replace(/^\//, "") ==
-  //         this.pathname.replace(/^\//, "") &&
-  //       location.hostname == this.hostname
-  //     ) {
-  //       var target = $(this.hash);
-  //       target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-  //       if (target.length) {
-  //         $("html, body").animate(
-  //           {
-  //             scrollTop: target.offset().top
-  //           },
-  //           800,
-  //           "easeInOutExpo"
-  //         );
-  //         return false;
-  //       }
-  //     }
-  //   }
-  // );
+  $('a[href*="#"]:not([href="#"])').click(
+    function() {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top
+            },
+            800,
+            "easeInOutExpo"
+          );
+          return false;
+        }
+      }
+    }
+  );
 
   // function scrollFunction() {
   //   document.body.scrollTop > 250 ? document.getElementById("top-btn").style.display = "flex" : document.getElementById("top-btn").style.display = "none";
@@ -79,34 +81,60 @@ $(document).ready(function(){
   //   scrollFunction();
   // })
   
-  $(document).scrollTop() > 250 && (document.getElementById("top-btn").style.display = "flex"), $("#top-btn").on(
-      "click",
-      function() {
-        $(".top-link").trigger("click");
-      }
-    ), $('a[href*="#"]')
-      .not('[href="#"]')
-      .not('[href="#0"]')
-      .click(function(e) {
-        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
-          var t = $(this.hash);
-          (t = t.length ? t : $("[name=" + this.hash.slice(1) + "]")), t.length && (e.preventDefault(), $("html, body").animate(
-                {
-                  scrollTop: t.offset().top
-                },
-                1e3,
-                function() {
-                  var e = $(t);
-                  return (
-                    e.focus(),
-                    !e.is(":focus") &&
-                      (e.attr("tabindex", "-1"), void e.focus())
-                  );
-                }
-              ));
-        }
-      });
+  // $(document).scrollTop() > 250 && (document.getElementById("top-btn").style.display = "flex"), $("#top-btn").on(
+  //     "click",
+  //     function() {
+  //       $(".top-link").trigger("click");
+  //     }
+    // ), 
+    // $('a[href*="#"]')
+    //   .not('[href="#"]')
+    //   .not('[href="#0"]')
+    //   .click(function(e) {
+    //     if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+    //       var t = $(this.hash);
+    //       (t = t.length ? t : $("[name=" + this.hash.slice(1) + "]")), t.length && (e.preventDefault(), $("html, body").animate(
+    //             {
+    //               scrollTop: t.offset().top
+    //             },
+    //             1e3,
+    //             function() {
+    //               var e = $(t);
+    //               return (
+    //                 e.focus(),
+    //                 !e.is(":focus") &&
+    //                   (e.attr("tabindex", "-1"), void e.focus())
+    //               );
+    //             }
+    //           ))
+    //     }
+    //   });
 
+  // $('a[href*="#"]')
+  //   .not('[href="#"]')
+  //   .not('[href="#0"]')
+  //   // Remove links that don't actually link to anything
+  //   .click(function(event) {
+  //     if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+  //       // Figure out element to scroll to
+  //       var target = $(this.hash);
+  //       target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+  //       // Does a scroll target exist?
+  //       if (target.length) {
+  //         // Only prevent default if animation is actually gonna happen
+  //         event.preventDefault();
+
+  //         var scrollTop = target.offset().top - $(".sticky-top").height();
+
+  //         $("html, body")
+  //           .animate(
+  //             { duration: 1000,
+  //               specialEasing: { 
+  //                 scrollTop: "easeOutBounce" 
+  //               }});
+  //       }
+  //     }
+  //   });
 
   $.fn.isInViewport = function() {
     var tony = $(this);
